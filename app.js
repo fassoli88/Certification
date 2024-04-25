32312c74c30473462e19e27627cb86b930650b74
fassoli88 <fassoli88@gmail.com>
2024-04-26 00:14:48 +0300

    Faisal Huge

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const passport = require('passport');
 const LocalStrategy = require("passport-local");
 const cert = require("./models/cert.js");
-const session = require('cookie-session');
+const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
 
 
@@ -19,17 +19,13 @@ app.use('/', router);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('trust proxy', 1);
-
 app.use(session({
-  cookie:{
-    secure: true,
-    maxAge:60000
-       },
-store: new RedisStore(),
-secret: 'secret',
-saveUninitialized: true,
-resave: false
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  resave: false,
+  secret: 'keyboard cat'
 }));
 
 app.use(function(req,res,next){
