decf28f9cedfb920e53582a129ac06292fc47ca0
fassoli88 <fassoli88@gmail.com>
2024-04-25 22:12:51 +0300

    Faisal Huge

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const passport = require('passport');
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
 const cert = require("./models/cert.js");
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
+
 
 
 app.set("view engine", "ejs");
@@ -20,6 +23,15 @@ app.use(require("express-session")({
     secret: "Rusty is a dog",
     resave: false,
     saveUninitialized: false
+}));
+
+app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: 'keyboard cat'
 }));
  
 app.use(passport.initialize());
