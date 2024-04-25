396717e9e54e89166c1a93cb7c84bb8461c234ed
fassoli88 <fassoli88@gmail.com>
2024-04-26 02:24:51 +0300

    Faisal Huge

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const passport = require('passport');
 const LocalStrategy = require("passport-local");
 const cert = require("./models/cert.js");
-require("dotenv").config();
+
 
 
 
