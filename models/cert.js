60dc4afb88bbe5107e97940b5d87a230a4605c36
fassoli88 <fassoli88@gmail.com>
2024-04-26 19:04:35 +0300

    Faisal Huge

--- models/cert.js
+++ models/cert.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+
 //mogodb
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cert')
 .then(()=>{
